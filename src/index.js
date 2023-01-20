0623c4a89ef57c1c43d1a3c965e0400e22a386e6
Alexey Gorchakov <amvrogor@gmail.com>
2023-01-20 11:22:25 +0200

    solving1

--- src/index.js
+++ src/index.js
@@ -4,34 +4,55 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('.search-form');
-// const submit = document.querySelector('button[type="submit"]');
 const gallery = document.querySelector('.gallery');
 const photoCard = document.querySelector('.photo-card');
 const loadMore = document.querySelector('.load-more');
 
+let page = 1;
+let per_page = 40;
+
 gallery.style.display = 'flex';
 gallery.style.flexWrap = 'wrap';
 gallery.style.gap = '10px';
-// photoCard.style.width = '300px';
+
+loadMore.hidden = true;
 
 form.addEventListener('submit', onSubmit);
 
-const SimpleLightboxModal = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 250,
-}).refresh();
+loadMore.addEventListener('click', onLoad);
 
 function onSubmit(e) {
   e.preventDefault();
   const query = e.currentTarget.elements[0].value.trim().toLowerCase();
-  pixaBayAPI(query)
-    // .then(data => console.log(data))
-    .then(data => (gallery.innerHTML = createMarkup(data)))
+  pixaBayAPI(query, page)
+    .then(data => {
+      if (data.hits.length === 0) {
+        gallery.innerHTML = '';
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        gallery.innerHTML = createMarkup(data);
+        loadMore.hidden = false;
+        Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        const SimpleLightboxModal = new SimpleLightbox('.gallery a').refresh();
+      }
+    })
+    .catch(err => console.log(err));
+  return query;
+}
+
+function onLoad() {
+  query = onSubmit();
+  page += 1;
+  pixaBayAPI(query, page)
+    .then(data => {
+      gallery.insertAdjacentHTML('beforeend', createMarkup(data));
+    })
     .catch(err => console.log(err));
 }
 
-async function pixaBayAPI(query) {
+async function pixaBayAPI(query, page) {
   const BASE_URL = 'https://pixabay.com/api/';
   const KEY = '32952504-2527882a1c0dde7bb411b7994';
   const searchParams = new URLSearchParams({
@@ -40,8 +61,11 @@ async function pixaBayAPI(query) {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
+    page: page,
+    per_page,
   });
   const response = await axios.get(`${BASE_URL}?${searchParams}`);
+
   return response.data;
 }
 
@@ -56,11 +80,9 @@ function createMarkup(data) {
         views,
         comments,
         downloads,
-
-        // totalHits,
       }) => `<div class="photo-card" style="width: 300px">
   <a class="gallery-img" href="${largeImageURL}">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" width=100% />
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width=100% height=50% style="display: block; object-fit: cover" />
   </a>
   <div class="info">
     <p class="info-item">
